7d064c7f8845257448199fc6994254b975d16783
jantonio2 <jesusrochavedia@gmail.com>
2023-03-16 23:41:46 -0400

    Create user routes and controllers

--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,24 @@
 import express, { Application } from 'express'
+import userRoutes from '../routes/user'
 
 class Server {
 
   private app: Application
   private port: string
+  private apiPaths = {
+    users: '/api/users'
+  }
 
   constructor() {
     this.app = express()
     this.port = process.env.PORT || '8000'
+
+    // Define routes
+    this.routes()
+  }
+
+  routes() {
+    this.app.use( this.apiPaths.users, userRoutes )
   }
 
   listen() {
